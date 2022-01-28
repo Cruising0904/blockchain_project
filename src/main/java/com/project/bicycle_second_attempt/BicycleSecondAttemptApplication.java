fc95536a76e60486ffe603f0fb8e6e467fec2cd0
Yong <cucrn35@gmail.com>
2022-01-28 16:36:43 +0900

    first commit

---
+++ src/main/java/com/project/bicycle_second_attempt/BicycleSecondAttemptApplication.java
@@ -0,0 +1,13 @@
+package com.project.bicycle_second_attempt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleSecondAttemptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleSecondAttemptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bicycle_second_attempt/BicycleSecondAttemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bicycle_second_attempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleSecondAttemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
